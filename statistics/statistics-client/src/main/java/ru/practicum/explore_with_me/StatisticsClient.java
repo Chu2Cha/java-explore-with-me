41755979a152a8e5693466ca6e5fd26b488ee8e3
Boris Chuguev <chuguevb@gmail.com>
2023-11-13 17:10:43 +0300

    stat_svc - ver 0.1 (modules, client - start)

---
+++ statistics/statistics-client/src/main/java/ru/practicum/explore_with_me/StatisticsClient.java
@@ -0,0 +1,38 @@
+package ru.practicum.explore_with_me;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.web.util.DefaultUriBuilderFactory;
+
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class StatisticsClient extends BaseClient{
+
+    private static final String SERVICE_URL = "url";
+    @Autowired
+    public StatisticsClient(@Value(SERVICE_URL) String serverUrl, RestTemplateBuilder builder) {
+        super(
+                builder
+                        .uriTemplateHandler(new DefaultUriBuilderFactory(serverUrl))
+                        .requestFactory(HttpComponentsClientHttpRequestFactory::new)
+                        .build()
+        );
+    }
+
+    public ResponseEntity<Object> getStatistics(String start, String end, List<String>uris, boolean unique) {
+        Map<String, Object> parameters = Map.of(
+                "start", start,
+                "end", end,
+                "uris", uris,
+                "unique", unique
+        );
+        return get("?start={start}&end={end}&uris={uris}&unique={unique}", parameters);
+    }
+
+}
---
+++ statistics/statistics-dto/src/main/java/ru/practicum/explore_with_me/statistics_dto/StatisticsDto.java
@@ -0,0 +1,4 @@
+package ru.practicum.explore_with_me.statistics_dto;
+
+public class StatisticsDto {
+}
